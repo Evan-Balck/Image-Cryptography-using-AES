781f10c1b34a9f4756589ee82f88c2da02bd0501
Mohit <mohitkala220103@gmail.com>
2023-07-17 11:56:00 +0530

    Decryption Done

--- encrypt.cpp
+++ encrypt.cpp
@@ -29,11 +29,37 @@ vector<vector<unsigned char>> SBox={
   {0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16}
 };
 
+vector<vector<unsigned char>> InvSBox={
+    {0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB},
+    {0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB},
+    {0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E},
+    {0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25},
+    {0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92},
+    {0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84},
+    {0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06},
+    {0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B},
+    {0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73},
+    {0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E},
+    {0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B},
+    {0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4},
+    {0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F},
+    {0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF},
+    {0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61},
+    {0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D}
+};
+
 const array<unsigned char, 16> mixColumnsMatrix = {
     0x02, 0x03, 0x01, 0x01,
     0x01, 0x02, 0x03, 0x01,
     0x01, 0x01, 0x02, 0x03,
     0x03, 0x01, 0x01, 0x02
+};
+
+const array<unsigned char, 16> invMixColumnsMatrix = {
+    0x0E, 0x0B, 0x0D, 0x09,
+    0x09, 0x0E, 0x0B, 0x0D,
+    0x0D, 0x09, 0x0E, 0x0B,
+    0x0B, 0x0D, 0x09, 0x0E
 };
 
 const array<unsigned char, 11> Rcon={
@@ -87,7 +113,7 @@ void ExpandKey(const unsigned char* originalKey, unsigned char* expandedKey) {
 }
 
 
-//Round function
+//Round function for encryption
 array<unsigned char, 16> SubBytes(const array<unsigned char, 16>& block) {
     array<unsigned char, 16> encryptedBlock;
 
@@ -148,6 +174,50 @@ array<unsigned char, 16> MixColumns(const array<unsigned char, 16>& block) {
 }
 
 
+//Round funtion for decryption
+array<unsigned char, 16> InvSubBytes(const array<unsigned char, 16>& block) {
+    array<unsigned char, 16> decryptedBlock;
+
+    for (int i = 0; i < 16; ++i) {
+        unsigned char pixel = block[i];
+        unsigned char decryptedPixel = InvSBox[pixel / 16][pixel % 16];
+        decryptedBlock[i] = decryptedPixel;
+    }
+
+    return decryptedBlock;
+}
+
+array<unsigned char, 16> InvMixColumns(const array<unsigned char, 16>& block) {
+    array<unsigned char, 16> decryptedBlock;
+
+    for (int col = 0; col < 4; ++col) {
+        for (int row = 0; row < 4; ++row) {
+            unsigned char result = 0;
+            for (int i = 0; i < 4; ++i) {
+                unsigned char multiplier = invMixColumnsMatrix[row * 4 + i];
+                unsigned char value = block[i * 4 + col];
+                result ^= MultiplyInGF2_8(multiplier, value);
+            }
+            decryptedBlock[row * 4 + col] = result;
+        }
+    }
+
+    return decryptedBlock;
+}
+
+array<unsigned char, 16> InvShiftRows(const array<unsigned char, 16>& block) {
+    array<unsigned char, 16> decryptedBlock;
+
+    for (int row = 0; row < 4; ++row) {
+        for (int col = 0; col < 4; ++col) {
+            int shiftedCol = (col - row + 4) % 4;
+            decryptedBlock[row * 4 + col] = block[row * 4 + shiftedCol];
+        }
+    }
+
+    return decryptedBlock;
+}
+
 
 
 void displayBlock(const array<unsigned char, 16>& block){
@@ -185,6 +255,9 @@ int main(){
     resize(image, image, Size(cols, rows));
 
     cvtColor(image,convertedImage,COLOR_BGR2GRAY);
+    namedWindow("Original Image", WINDOW_NORMAL);
+    imshow("Original Image", convertedImage);
+    waitKey(0);
 
 
     const int blockSize=16;
@@ -204,8 +277,8 @@ int main(){
         }
     }
 
-    //AES encrytpion functions calls
-    for (const auto& block : blocks) {
+    //AES encrytpion calls
+    for (auto& block : blocks) {
         array<unsigned char, 16> state = block;
 
         for (int i = 0; i < 16; ++i) {
@@ -229,10 +302,63 @@ int main(){
             state[i] ^= expandedKey[160 + i];
         }
 
-        displayBlock(state);
+        block = state;
+
+        //displayBlock(state);
+
     }
 
 
+    //AES decryption calls
+    for (auto& block : blocks) {
+        array<unsigned char, 16> state = block;
+
+        for (int i = 0; i < 16; ++i) {
+            state[i] ^= expandedKey[160 + i];
+        }
+
+        state = InvShiftRows(state);
+        state = InvSubBytes(state);
+
+        for (int round = 9; round >= 1; --round) {
+            for (int i = 0; i < 16; ++i) {
+                state[i] ^= expandedKey[(round * 16) + i];
+            }
+
+            state = InvMixColumns(state);
+            state = InvShiftRows(state);
+            state = InvSubBytes(state);
+        }
+
+        for (int i = 0; i < 16; ++i) {
+            state[i] ^= expandedKey[i];
+        }
+
+        block = state;
+        //displayBlock(state);
+    }
+
+    Mat decryptedImage(image.size(), CV_8UC1);
+
+    int blockIndex = 0;
+
+    for (int i = 0; i < convertedImage.rows; i += 4) {
+        for (int j = 0; j < convertedImage.cols; j += 4) {
+            const auto& block = blocks[blockIndex];
+
+            for (int k = 0; k < 4; ++k) {
+                for (int l = 0; l < 4; ++l) {
+                    decryptedImage.at<unsigned char>(i + k, j + l) = block[k * 4 + l];
+                }
+            }
+
+            blockIndex++;
+        }
+    }
+
+    namedWindow("Decrypted Image", WINDOW_NORMAL);
+    imshow("Decrypted Image", decryptedImage);
+    waitKey(0);
 
     return 0;
 }
